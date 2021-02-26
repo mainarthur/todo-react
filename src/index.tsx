4e1cf62a007db4962de5eae8dd5ea169daffde72
Arthur Kh <listbox6@gmail.com>
2021-02-26 17:57:06 +0200

    protectedroute

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom"
 import App from "./App";
+import ProtectedRoute from "./common/ProtectedRoute";
 import Login from "./login/Login";
 import Register from "./register/Register";
 
 ReactDOM.render(
 	<BrowserRouter>
 	<Switch>
-		<Route exact path="/" component={App}/>
-		<Route exact path="/login" component={Login}/>
-		<Route exact path="/register" component={Register}/>
+		<ProtectedRoute exact path="/" redirectPath="/login" condition={localStorage.getItem("access_token") != null} component={App}/>
+		<ProtectedRoute exact path="/login" redirectPath="/" condition={localStorage.getItem("access_token") == null} component={Login}/>
+		<ProtectedRoute exact path="/register" redirectPath="/" condition={localStorage.getItem("access_token") == null} component={Register}/>
 	</Switch>
 	</BrowserRouter>,
 	document.getElementById("root")
