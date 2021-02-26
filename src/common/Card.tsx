69f9ee93c45f1e8afe1569c3861197051fe41417
Arthur Kh <listbox6@gmail.com>
2021-02-26 18:44:08 +0200

    Card component

---
+++ src/common/Card.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+type CardProps = {
+
+}
+
+type CardState = {
+
+}
+
+class Card extends React.Component<CardProps, CardState> {
+	constructor(props: CardProps | Readonly<CardProps>) {
+		super(props)
+	}
+
+	render(): JSX.Element {
+		return <div></div>
+	}
+	
+}
+export default Card
