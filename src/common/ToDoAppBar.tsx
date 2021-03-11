fe47cedfe6ebd4d596cae7434550dda33f7911f6
Arthur Kh <listbox6@gmail.com>
2021-03-11 11:14:04 +0200

    todoappbar in fc

--- src/common/ToDoAppBar.tsx
+++ src/common/ToDoAppBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import AppBar from '@material-ui/core/AppBar'
 import IconButton from '@material-ui/core/IconButton'
@@ -10,54 +10,35 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 
 import { deleteTokensAction } from '../redux/actions/tokenActions'
 import { RootState } from '../redux/reducers'
-import { TokensState } from '../redux/reducers/tokensReducer'
 
 import { history } from '../routing/RouterContext'
+import useStyle from './ToDoAppBarStyles'
 
-interface DispatchProps {
-  deleteTokens: typeof deleteTokensAction
-}
+const ToDoAppBar: React.FC = () => {
+  const classes = useStyle()
+  const { accessToken } = useSelector((state: RootState) => state.tokens)
+  const dispatch = useDispatch()
 
-type Props = DispatchProps & TokensState
-
-class ToDoAppBar extends React.PureComponent<Props> {
-  onLogOutClick = () => {
-    const { deleteTokens } = this.props
-
-    deleteTokens()
+  const onLogOutClick = () => {
+    dispatch(deleteTokensAction())
     localStorage.clear()
     history.push('/login')
   }
 
-  render() {
-    const { classes, accessToken } = this.props
-
-    return (
-      <AppBar>
-        <Toolbar className={classes.toolBar}>
-          <Typography className={classes.title} variant="h5">
-            To-Do List
-          </Typography>
-          {accessToken !== '' ? (
-            <IconButton onClick={this.onLogOutClick}>
-              <ExitToAppIcon className={classes.logOutIcon} />
-            </IconButton>
-          ) : null}
-        </Toolbar>
-      </AppBar>
-    )
-  }
+  return (
+    <AppBar>
+      <Toolbar className={classes.toolBar}>
+        <Typography className={classes.title} variant="h5">
+          To-Do List
+        </Typography>
+        {accessToken !== '' ? (
+          <IconButton onClick={onLogOutClick}>
+            <ExitToAppIcon className={classes.logOutIcon} />
+          </IconButton>
+        ) : null}
+      </Toolbar>
+    </AppBar>
+  )
 }
 
-const mapStateToProps = (state: RootState): TokensState => ({
-  ...state.tokens,
-})
-
-const mapDispatchToProps: DispatchProps = {
-  deleteTokens: deleteTokensAction,
-}
-
-export default connect<TokensState, DispatchProps>(
-  mapStateToProps,
-  mapDispatchToProps,
-)(withStyles(styles)(ToDoAppBar))
+export default ToDoAppBar
