8c90ddc36e62590119c83d18cd49d672369cce41
Arthur Kh <listbox6@gmail.com>
2021-03-18 18:20:18 +0200

    upd

--- src/redux/actions/toDoActions.ts
+++ src/redux/actions/toDoActions.ts
@@ -8,14 +8,15 @@ import UpdateToDoBody from '../../api/bodies/UpdateToDoBody'
 import DeleteManyBody from '../../api/bodies/DeleteManyBody'
 import DeleteToDoPayload from '../types/payloads/DeleteToDoPayload'
 import SetLoadingPartPayload from '../types/payloads/SetLoadingPartPayload'
+import BoardPayload from '../types/payloads/BoardPayload'
 
-export const setTodosAction = createAction<ToDo[]>(ToDoAction.SET_TODOS)
+export const setTodosAction = createAction<ToDo[] & BoardPayload>(ToDoAction.SET_TODOS)
 
 export const newToDoAction = createAction<NewToDoBody>(ToDoAction.REQUEST_NEW_TODO)
 
-export const addToDoAction = createAction<ToDo>(ToDoAction.ADD_TODO)
+export const addToDoAction = createAction<ToDo & BoardPayload>(ToDoAction.ADD_TODO)
 
-export const requestTodosAction = createAction(ToDoAction.REQUEST_TODOS)
+export const requestTodosAction = createAction<BoardPayload>(ToDoAction.REQUEST_TODOS)
 
 export const updateToDoAction = createAction<UpdateToDoBody>(ToDoAction.REQUEST_UPDATE_TODO)
 
@@ -23,8 +24,10 @@ export const deleteManyToDosAction = createAction<DeleteManyBody>(
   ToDoAction.REQUEST_DELETE_MANY_TODOS,
 )
 
-export const deleteToDoAction = createAction<DeleteToDoPayload>(
+export const deleteToDoAction = createAction<DeleteToDoPayload & BoardPayload>(
   ToDoAction.REQUEST_DELETE_TODO,
 )
 
-export const setLoadingPartAction = createAction<SetLoadingPartPayload>(ToDoAction.SET_LOADING_PART)
+export const setLoadingPartAction = createAction<SetLoadingPartPayload & BoardPayload>(
+  ToDoAction.SET_LOADING_PART,
+)
--- src/redux/reducers/boardsReducer.ts
+++ src/redux/reducers/boardsReducer.ts
@@ -1,33 +1,47 @@
 import ToDo from '../../models/ToDo'
 import Action from '../types/Action'
 import { addToDoAction, setLoadingPartAction, setTodosAction } from '../actions/toDoActions'
+import Board from '../../models/Board'
+import todosReducer from './todosReducer'
 
-export type TodosState = ToDo[]
+export type BoardsState = Array<Board & { todos: ToDo[] }>
 
-const initialState: TodosState = []
+const initialState: BoardsState = []
 
-export default function todosReducer(state = initialState, action: Action): TodosState {
+export default function boardsReducer(state = initialState, action: Action): BoardsState {
   let newState = [...state]
 
   if (addToDoAction.match(action)) {
-    newState = newState.concat([{ ...action.payload }])
+    newState = newState.map((board) => {
+      const { _id: boardId } = board
+      if (action.payload.boardId === boardId) {
+        const newBoard = { ...board, todos: todosReducer(board.todos, action) }
+
+        return newBoard
+      }
+      return board
+    })
   }
   if (setTodosAction.match(action)) {
-    newState = [...action.payload]
+    newState = newState.map((board) => {
+      const { _id: boardId } = board
+      if (action.payload.boardId === boardId) {
+        const newBoard = { ...board, todos: todosReducer(board.todos, action) }
+
+        return newBoard
+      }
+      return board
+    })
   }
   if (setLoadingPartAction.match(action)) {
-    const {
-      payload: {
-        ids,
-        loadingPart,
-      },
-    } = action
-    newState = newState.map((toDo) => {
-      const { _id: toDoId } = toDo
-      if (ids.indexOf(toDoId) !== -1) {
-        return { ...toDo, loadingPart }
+    newState = newState.map((board) => {
+      const { _id: boardId } = board
+      if (action.payload.boardId === boardId) {
+        const newBoard = { ...board, todos: todosReducer(board.todos, action) }
+
+        return newBoard
       }
-      return toDo
+      return board
     })
   }
 
